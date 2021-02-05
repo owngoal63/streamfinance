294362f206642c22475946fad492f07437c6b7d4
owngoal63 <gordonlindsay@virginmedia.com>
2021-02-05 23:37:55 +0000

    Updated app2 with buy sell indicators

--- finance_app2.py
+++ finance_app2.py
@@ -9,6 +9,37 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
 
+def buy_sell(data):
+    sigPriceBuy = []
+    sigPriceSell = []
+    flag = -1
+
+    for i in range(len(data)):
+        if data['SMA30'][i] > data['SMA100'][i]:
+            if flag != 1:
+                sigPriceBuy.append(data['Daily Close'][i])
+                sigPriceSell.append(np.nan)
+                flag = 1
+            else:
+                sigPriceBuy.append(np.nan)
+                sigPriceSell.append(np.nan)
+        elif data['SMA30'][i] < data['SMA100'][i]:
+            if flag != 0:
+                sigPriceBuy.append(np.nan)
+                sigPriceSell.append(data['Daily Close'][i])
+                flag = 0
+            else:
+                sigPriceBuy.append(np.nan)
+                sigPriceSell.append(np.nan)
+        else:
+            sigPriceBuy.append(np.nan)
+            sigPriceSell.append(np.nan)
+
+    return (sigPriceBuy, sigPriceSell)
+
+
+
+
 st.sidebar.header("NYSE Stock Tickers")
 tickerSymbols = st.sidebar.multiselect(
     "Select ticker:",
@@ -59,6 +90,30 @@ for tickerSymbol in tickerSymbols:
     ax.legend(loc='upper left')
 
     st.pyplot(fig)
+
+    # Create new data frame to store all the data
+    data = pd.DataFrame()
+    data['Daily Close'] = tickerDf['Close']
+    data['SMA30'] = SMA30['Close']
+    data['SMA100'] = SMA100['Close']
+    buy_sell_data = buy_sell(data)
+    data['Buy Signal Price'] = buy_sell_data[0]
+    data['Sell Signal Price'] = buy_sell_data[1]
+    # Show the consolidated Dataframe
+    #st.dataframe(data)  
+
+    # Visualise the data and the strategy
+    fig, ax = plt.subplots()
+    ax.plot(data['Daily Close'], label='Daily Close Price ($)', alpha = 0.35)
+    ax.plot(data['SMA30'], label='SMA30', alpha = 0.35)
+    ax.plot(data['SMA100'], label='SMA100', alpha = 0.35)
+    ax.scatter(data.index, data['Buy Signal Price'], label = "Buy", marker='^', color='green')
+    ax.scatter(data.index, data['Sell Signal Price'], label = "Sell", marker='v', color='red')
+    ax.legend(loc='upper left')
+    st.pyplot(fig)
+
+
+      
 
 
 
